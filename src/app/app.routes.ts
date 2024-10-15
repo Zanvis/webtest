59f00d9fdbc341cffa038af50e2f505d16480c07
Antoni Piwowarski <legoo1312@gmail.com>
2024-10-15 11:48:32 +0200

    2nd try

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SongListComponent } from './components/song-list/song-list.component';
 import { SongUploadComponent } from './components/song-upload/song-upload.component';
 import { TeamComponent } from './components/team/team.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
 
 export const routes: Routes = [
     // { path: '', redirectTo: '/songs', pathMatch: 'full' },
@@ -10,5 +11,6 @@ export const routes: Routes = [
     { path: 'songs', component: SongListComponent },
     { path: 'upload', component: SongUploadComponent },
     { path: 'team', component: TeamComponent },
+    { path: 'playlists', component: PlaylistComponent },
     { path: '**', redirectTo: '/songs' }
 ];
